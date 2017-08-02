0cdb19d61a4133cba3aa4a1053f85dfbfd79ddaa
Kieve Chua <kievechua@gmail.com>
2017-08-02 14:28:18 +0800

    add missing module

---
+++ dist/config/graphql_server.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    resolversDir: 'src/routers/graphql',
+    schemasDir: 'src/schemas',
+    scalarFile: 'src/schemas/_scalar'
+};
+//# sourceMappingURL=graphql_server.js.map
